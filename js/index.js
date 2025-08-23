87729f499e0904054daa3fe452faa51d6f1ca1bd
Annelise <annelise.celma@gmail.com>
2025-08-23 11:37:26 -0400

    rectification countdown

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ function updateTimer(){
     var now = new Date().getTime();
     var distance = countDownDate-now;
 
-    var days = Math.floor(distance/(100*60*60*24));
+    var days = Math.floor(distance/(1000*60*60*24));
     var hours = Math.floor((distance % (1000*60*60*24))/(1000*60*60));
     var minutes = Math.floor((distance% (1000*60*60))/(1000*60));
     var seconds = Math.floor((distance%(1000*60))/1000);
